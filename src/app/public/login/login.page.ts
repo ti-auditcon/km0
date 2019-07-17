7a31b71e58837363b65a16a086415212e0e425ea
Juan <jberrioscifuentes@gmail.com>
2019-07-17 12:02:21 -0400

    Login y Forget

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
