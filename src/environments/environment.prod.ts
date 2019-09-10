c85900b3d49c03358da6b933650a6ca07ea85bf9
boryeira <jmanuel.jorquera@gmail.com>
2019-09-10 18:25:49 -0300

    shit 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true
 };
 
-export const SERVER_URL = 'http://km0-api.asomic.com/';
-// export const SERVER_URL = 'http://km0-api.test';
+export const SERVER_URL = 'https://api.bicicleteriakilometrocero.com';
+//export const SERVER_URL = 'http://km0-api.test';
+
 export const API_KEY = 'V6LC9Ks2Ashb6UtpVc3ef9LUVoAFokF479P2E5xG';
+
